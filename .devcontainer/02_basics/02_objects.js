176aaf316cc167dbe13c6aad1ad49264f3c296d5
Mohitgreatsword07 <147631704+Mohitgreatsword07@users.noreply.github.com>
2024-11-14 12:21:28 +0000

    02_objects.js

---
+++ .devcontainer/02_basics/02_objects.js
@@ -0,0 +1,16 @@
+//  whenever we declare objects in the form of literals, then singleton will not form
+//  but when we use constructor , then singleton will be form
+singleton
+//object.create yehi hai constructor method ki through
+// object literals
+const jsuser = {
+    name: "mohit" ,
+    location: "delhi",
+    email: "mohit@gmail.com"
+
+}
+//how to access an object
+console.log(jsuser.email)
+console.log(jsuser["email"]);
+
+
